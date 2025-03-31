3d0e59bf74cd2f2a7f7d37b779900b356bcac872
Colvyn Harris Mathan Mohan <81992703+colvyn01@users.noreply.github.com>
2025-03-31 10:44:06 -0400

    Update NickelTest.java

--- NickelTest.java
+++ NickelTest.java
@@ -1,1 +1,23 @@
-
+class NickelTest {
+    @Test
+    void testNickelDefaultConstructor() {
+        Nickel nickel = new Nickel();
+        assertEquals(0.05, nickel.getValue());
+        assertEquals("Nickel", nickel.getCommonName());
+        assertEquals("IN GOD WE TRUST", nickel.getFrontMotto());
+        assertEquals("T_Jefferson", nickel.getFrontImage());
+        assertEquals("Jefferson_Memorial", nickel.getBackImage());
+        assertEquals("E PLURIBUS UNUM", nickel.getBackMotto());
+        assertEquals("LIBERTY", nickel.getFrontLabel());
+        assertEquals("UNITED STATES OF AMERICA", nickel.getBackLabel());
+        assertEquals("FIVE CENTS", nickel.getValueDescription());
+        assertFalse(nickel.getRidgedEdge());
+        assertEquals("Cupro-Nickel", nickel.getMetallurgy());
+    }
+    
+    @Test
+    void testNickelYearConstructor() {
+        Nickel nickel = new Nickel(2000);
+        assertEquals(2000, nickel.getManufactureYear());
+    }
+}
