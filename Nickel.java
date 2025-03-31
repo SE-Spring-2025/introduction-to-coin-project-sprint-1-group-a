a0f7c4a055cd1c8b876d549e83255bc8c3c28d5a
Colvyn Harris Mathan Mohan <81992703+colvyn01@users.noreply.github.com>
2025-03-31 10:43:37 -0400

    Update Nickel.java

--- Nickel.java
+++ Nickel.java
@@ -1,1 +1,9 @@
-
+public class Nickel extends Coin {
+    public Nickel() {
+        super(0.05, "Nickel", "IN GOD WE TRUST", 2024, "T_Jefferson", "Jefferson_Memorial", "E PLURIBUS UNUM", "LIBERTY", "UNITED STATES OF AMERICA", "FIVE CENTS", false, "Cupro-Nickel");
+    }
+    
+    public Nickel(int year) {
+        super(0.05, "Nickel", "IN GOD WE TRUST", year, "T_Jefferson", "Jefferson_Memorial", "E PLURIBUS UNUM", "LIBERTY", "UNITED STATES OF AMERICA", "FIVE CENTS", false, "Cupro-Nickel");
+    }
+}
