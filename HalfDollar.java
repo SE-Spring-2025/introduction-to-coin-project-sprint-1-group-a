5cacce435f3ef412f65ab1e8a976a074de0f660c
Colvyn Harris Mathan Mohan <81992703+colvyn01@users.noreply.github.com>
2025-03-31 10:41:54 -0400

    Update HalfDollar.java

--- HalfDollar.java
+++ HalfDollar.java
@@ -1,1 +1,11 @@
+public class HalfDollar extends Coin {
+    public HalfDollar() {
+        this(Calendar.getInstance().get(Calendar.YEAR)); // Default to current year
+    }
 
+    public HalfDollar(int year) {
+        super(0.50, "HalfDollar", "IN GOD WE TRUST", year, "J_Kennedy", "Presidential_Seal", 
+              "E PLURIBUS UNUM", "LIBERTY", "UNITED STATES OF AMERICA", 
+              "HALF DOLLAR", true, "Cupro-Nickel");
+    }
+}
