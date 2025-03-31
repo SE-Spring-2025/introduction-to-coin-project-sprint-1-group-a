a059d7313f03e0939ad22cd1465c13e3cbc6185f
Colvyn Harris Mathan Mohan <81992703+colvyn01@users.noreply.github.com>
2025-03-31 11:07:15 -0400

    Update Quarter.java

--- Quarter.java
+++ Quarter.java
@@ -1,1 +1,21 @@
+public class Quarter extends Coin {
 
+    public Quarter() {
+        this(Calendar.getInstance().get(Calendar.YEAR));
+    }
+
+    public Quarter(int manufactureYear) {
+        super(0.25,
+                "Quarter",
+                "IN GOD WE TRUST",
+                manufactureYear,
+                "G_Washington",
+                "Eagle",
+                "E PLURIBUS UNUM",
+                "LIBERTY",
+                "UNITED STATES OF AMERICA",
+                "QUARTER DOLLAR",
+                true,
+                "Cupro-Nickel");
+    }
+}
