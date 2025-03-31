ac167c248cfe23bee217c6001d71f2e90c6a5424
Colvyn Harris Mathan Mohan <81992703+colvyn01@users.noreply.github.com>
2025-03-31 10:42:17 -0400

    Update HalfDollarTest.java

--- HalfDollarTest.java
+++ HalfDollarTest.java
@@ -1,1 +1,17 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class HalfDollarTest {
+    @Test
+    public void testHalfDollarConstructor() {
+        HalfDollar h = new HalfDollar(2022);
+        assertEquals(0.50, h.getValue());
+        assertEquals("HalfDollar", h.getCommonName());
+        assertEquals("J_Kennedy", h.getFrontImage());
+        assertEquals("Presidential_Seal", h.getBackImage());
+        assertEquals("HALF DOLLAR", h.getValueDescription());
+        assertTrue(h.getRidgedEdge());
+        assertEquals("Cupro-Nickel", h.getMetallurgy());
+    }
+}
 
