f228270efd7ea7e6cae96d30004040dd638ea847
colvyn01 <colvyn01@gmail.com>
2025-04-06 14:12:27 -0400

    Updated Dime.java

--- Dime.java
+++ Dime.java
@@ -1,11 +1,21 @@
+import java.util.Calendar;
+
 public class Dime extends Coin {
     public Dime() {
-        this(Calendar.getInstance().get(Calendar.YEAR)); // Default to current year
+        this(Calendar.getInstance().get(Calendar.YEAR));
     }
 
     public Dime(int year) {
-        super(0.10, "Dime", "IN GOD WE TRUST", year, "F_Roosevelt", "Torch_Branches", 
-              "E PLURIBUS UNUM", "LIBERTY", "UNITED STATES OF AMERICA", 
-              "ONE DIME", true, "Cupro-Nickel");
+        super(0.10, year);
+
+        setCommonName("Dime");
+        setFrontImage("F_Roosevelt");
+        setBackImage("Torch_Branches");
+        setBackMotto("E PLURIBUS UNUM");
+        setFrontLabel("LIBERTY");
+        setBackLabel("UNITED STATES OF AMERICA");
+        setValueDescription("ONE DIME");
+        setRidgedEdge(true);
+        setMetallurgy("Cupro-Nickel");
     }
 }
